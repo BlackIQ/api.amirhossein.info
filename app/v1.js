f00d7a39c41e8d463e64defc98f586ac48cc034f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-20 23:58:36 +0430

    Add views route

--- app/v1.js
+++ app/v1.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const ViewRoutes = require("./routes/view.routes");
 const MessageRoutes = require("./routes/message.routes");
+const AuthenticatioRoutes = require("./routes/authentication.routes");
 
 const app = express();
 
+app.use("/view", ViewRoutes);
 app.use("/message", MessageRoutes);
+app.use("/authentication", AuthenticatioRoutes);
 
 module.exports = app;
