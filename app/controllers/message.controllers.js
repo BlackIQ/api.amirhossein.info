8e115e2b905bc702f31c81a7f2ef25f7e210841b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-20 21:30:30 +0430

    Add dekete controller

--- app/controllers/message.controllers.js
+++ app/controllers/message.controllers.js
@@ -6,11 +6,18 @@ const create = (req, res) => {
     const newMessage = new Message(data);
     newMessage.save()
         .then((saved) => {
-            const sendingData = {
-                message: "Message saved",
-                id: saved.id,
-            };
-            res.status(200).send(sendingData);
+            if (data.name !== "" && data.message !== "") {
+                const sendingData = {
+                    message: "Message saved",
+                    id: saved.id,
+                };
+                res.status(200).send(sendingData);                
+            } else {
+                const sendingData = {
+                    error: "Sorry there is an error occurred. Can you ty again!?",
+                }
+                res.status(403).send(sendingData);
+            }
         })
         .catch((error) => {
             const sendingData = {
@@ -54,8 +61,27 @@ const readOne = (req, res) => {
         });
 }
 
+const deleteOne = (req, res) =>{
+    const { id } = req.params;
+
+    Message.findByIdAndDelete(id)
+        .then((deleted) => {
+            const sendingData = {
+                message: "Message deleted",
+            };
+            res.status(200).send(sendingData);
+        })
+        .catch((error) => {
+            const sendingData = {
+                error: error.message,
+            };
+            res.status(400).send(sendingData);
+        });
+}
+
 module.exports = {
     create,
     readAll,
     readOne,
+    deleteOne,
 }
