0bd583d1480b61da986c34142b89a42f4bc19f46
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-20 21:30:46 +0430

    Add delete route

--- app/routes/message.routes.js
+++ app/routes/message.routes.js
@@ -7,5 +7,6 @@ const Router = express.Router();
 Router.post("/", controllers.create);
 Router.get("/", controllers.readAll);
 Router.get("/:id", controllers.readOne);
+Router.delete("/:id", controllers.deleteOne);
 
 module.exports = Router;
