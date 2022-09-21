7132a4f3df42bda363297b769c153af2f1a7d829
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-21 22:34:24 +0430

    Fix issues

--- app/controllers/authentication.controllers.js
+++ app/controllers/authentication.controllers.js
@@ -3,53 +3,61 @@ const User = require("../models/user.model");
 const login = (req, res) => {
     const data = req.body;
 
-    User.find(data)
-        .then((user) => {
-            if (user === null) {
+    if (data.username !== "" && data.password !== "") {
+        User.findOne(data)
+            .then((user) => {
+                if (user === null) {
+                    const sendingData = {
+                        error: "Sorry, but user is not founded.",
+                    };
+                    res.status(401).send(sendingData);
+                } else {
+                    const sendingData = {
+                        id: user.id,
+                    };
+                    res.status(200).send(sendingData);
+                    
+                }
+            })
+            .catch((error) => {
                 const sendingData = {
-                    error: "Sorry, but user is not founded.",
+                    error: error.message,
                 };
-                res.status(401).send(sendingData);
-            } else {
-                const sendingData = {
-                    id: user.id,
-                };
-                res.status(200).send(sendingData);
-                
-            }
-        })
-        .catch((error) => {
-            const sendingData = {
-                error: error.message,
-            };
-            res.status(400).send(sendingData);
-        });
+                res.status(400).send(sendingData);
+            });
+    } else {
+        const sendingData = {
+            error: "Fill all blanks first.",
+        };
+        res.status(401).send(sendingData);
+    }
 }
 
 const register = (req, res) => {
     const data = req.body;
 
-    const newUser = new User(data);
-    newUser.save()
-        .then((user) => {
-            if (data.username !== "" && data.password !== "") {
+    if (data.username !== "" && data.password !== "") {
+        const newUser = new User(data);
+        newUser
+            .save()
+            .then((user) => {
                 const sendingData = {
                     id: user.id,
                 };
-                res.status(200).send(sendingData);                
-            } else {
+                res.status(200).send(sendingData);
+            })
+            .catch((error) => {
                 const sendingData = {
-                    error: "Sorry there is an error occurred. Can you ty again!?",
-                }
-                res.status(403).send(sendingData);
-            }
-        })
-        .catch((error) => {
-            const sendingData = {
-                error: error.message,
-            };
-            res.status(400).send(sendingData);
-        });
+                    error: error.message,
+                };
+                res.status(400).send(sendingData);
+            });
+    } else {
+        const sendingData = {
+            error: "Sorry there is an error occurred. Can you ty again!?",
+        };
+        res.status(401).send(sendingData);
+    }
 }
 
 module.exports = {
