8bc51745f24f3200d639597adf108ae57758df58
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-11-06 15:28:15 +0330

    Fix auth

--- app/controllers/authentication/authentication.controllers.js
+++ app/controllers/authentication/authentication.controllers.js
@@ -3,33 +3,24 @@ import { UserModel as User } from "$models/index.js";
 export const login = (req, res) => {
   const data = req.body;
 
-  if (data.username !== "" && data.password !== "") {
-    User.findOne(data)
-      .then((user) => {
-        if (user === null) {
-          const sendingData = {
-            error: "Sorry, but user is not founded.",
-          };
-          res.status(401).send(sendingData);
-        } else {
-          const sendingData = {
-            id: user.id,
-          };
-          res.status(200).send(sendingData);
-        }
-      })
-      .catch((error) => {
-        const sendingData = {
-          error: error.message,
-        };
-        res.status(400).send(sendingData);
-      });
-  } else {
-    const sendingData = {
-      error: "Fill all blanks first.",
-    };
-    res.status(401).send(sendingData);
-  }
+  User.findOne(data)
+    .then((user) => {
+      if (user) {
+        res.status(401).send({
+          error: "Sorry, but user is not founded.",
+        });
+      } else {
+        res.status(200).send({
+          id: user.id,
+        });
+      }
+    })
+    .catch((error) => {
+      const sendingData = {
+        error: error.message,
+      };
+      res.status(400).send(sendingData);
+    });
 };
 
 export const register = (req, res) => {
