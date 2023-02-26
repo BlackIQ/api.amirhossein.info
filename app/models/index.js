31dc7ac894a9c936f4092c2d23155b06ddcc99d9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-26 11:44:25 +0330

    Delete models

--- app/models/index.js
+++
@@ -1,5 +0,0 @@
-import MessageModel from "$models/message/message.model.js";
-import UserModel from "$models/user/user.model.js";
-import LogModel from "$models/log/log.model.js";
-
-export { MessageModel, LogModel, UserModel };
