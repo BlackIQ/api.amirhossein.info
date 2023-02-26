5dda1ef6a2cb63483ab1d3490e0e857fce9bfe6a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-26 11:44:46 +0330

    Done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import app from "./app/index.js";
 
 import { appConfig } from "$config/index.js";
-import { mongodb } from "$connections/index.js";
 
 app.listen(appConfig.port, async () => {
   console.log(`App is running on ${appConfig.port}`);
