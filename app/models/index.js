b888e1514e38b3ad41149ff54c66ff8c5e8df2f7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-31 00:03:02 +0330

    Export stuff

--- app/models/index.js
+++ app/models/index.js
@@ -1,3 +1,7 @@
 import Social from "$models/social/social.model.js";
+import Duty from "$models/duty/duty.model.js";
+import Skill from "$models/skill/skill.model.js";
+import PersonalInfo from "$models/personal/personal.model.js";
+import Experience from "$models/experience/experience.model.js";
 
-export { Social };
+export { Social, Duty, Skill, PersonalInfo, Experience };
