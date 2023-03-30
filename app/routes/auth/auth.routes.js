c472d10f8201e9cd326e7dcc4548c2676fafdf08
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-31 00:02:38 +0330

    Delete stuff

--- app/routes/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Auth } from "$controllers/index.js";
-
-const router = express.Router();
-
-router.post("/login", Auth.LOGIN);
-router.post("/register", Auth.REGISTER);
-
-export default router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import Notes from "$routes/notes/notes.routes.js";
-import Auth from "$routes/auth/auth.routes.js";
+import Duty from "$routes/duty/duty.routes.js";
+import Experience from "$routes/experience/experience.routes.js";
+import Personal from "$routes/personal/personal.routes.js";
+import Skill from "$routes/skill/skill.routes.js";
+import Social from "$routes/social/social.routes.js";
 
 const Router = express();
 
-Router.use("/notes", Notes);
-Router.use("/auth", Auth);
+Router.use("/duties", Duty);
+Router.use("/experiences", Experience);
+Router.use("/personals", Personal);
+Router.use("/skills", Skill);
+Router.use("/socials", Social);
 
 export default Router;
