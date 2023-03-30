5f7799d6b7221c9918a901a99044185572ac22f1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-30 19:36:58 +0330

    Remove some connections

--- app/connections/index.js
+++ app/connections/index.js
@@ -5,7 +5,5 @@
 // Like configs, create them in directories and export them here.
 
 import MongoDB from "$connections/mongodb/mongodb.connection.js";
-import MySQL from "$connections/mysql/mysql.connection.js";
-import Redis from "$connections/redis/redis.connection.js";
 
-export { MongoDB, MySQL, Redis };
+export { MongoDB };
