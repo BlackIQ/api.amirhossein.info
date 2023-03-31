068de5d79091c21e78777a948d6dc3fc95b460d7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-31 07:09:16 +0330

    Comment all

--- app/middlewares/ip/ip.middleware.js
+++ app/middlewares/ip/ip.middleware.js
@@ -11,12 +11,10 @@ const ip = (req, res, next) => {
   // Here we find comming ip.
   // Then check that ip is white or not by finding in it whitelist.
 
-  const headers = req.headers;
-  const whitelist = [];
+  // const headers = req.headers;
+  // const whitelist = [];
 
-  const coming = headers["x-forwarded-for"];
-
-  console.log(headers);
+  // const coming = headers["x-forwarded-for"];
 
   // if (!whitelist.includes(coming)) {
   //   return res.status(401).send({ message: "Unauthorized" });
