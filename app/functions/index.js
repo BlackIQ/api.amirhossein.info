377701ef8abeb205bbfef1b49a2667c7635fe2d1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-02-04 19:00:12 +0330

    Functions updated

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,10 +1,11 @@
 // ----------------------------------------------
+// $app/functions
 // index.js
 // ----------------------------------------------
 // Exporting all functions.
 // To use functions that created in subdirectories, we export them here.
 
-import { ray } from "$functions/ray/ray.function.js";
-import { createToken, reverseToken } from "$functions/token/token.function.js";
+import { intRay, ray } from "$app/functions/ray/ray.function.js";
+import createToken from "$app/functions/token/token.function.js";
 
-export { ray, createToken, reverseToken };
+export { ray, intRay, createToken };
--- app/functions/token/token.function.js
+++ app/functions/token/token.function.js
@@ -1,15 +1,15 @@
 // ----------------------------------------------
+// $app/functions/token
 // token.function.js
 // ----------------------------------------------
 // Token generator.
 // We use JWT for authorization. To create the token, we use this function.
 
-import { appConfig } from "$config";
-import { User } from "$models";
+import { appConfig } from "$app/config/index.js";
 
 import jwt from "jsonwebtoken";
 
-const createToken = (id) => {
+const createToken = (payload) => {
   // ----------------------------------------------
   // createToken()
   // ----------------------------------------------
@@ -17,15 +17,7 @@ const createToken = (id) => {
   // Find secret in appConfig.
   // Then set expiresIn for 7 days.
 
-  return jwt.sign({ id }, appConfig.secret, { expiresIn: "7d" });
+  return jwt.sign(payload, appConfig.secret, { expiresIn: "30d" });
 };
 
-const reverseToken = async (authorization) => {
-  const token = authorization.split(" ")[1];
-
-  const { id } = jwt.verify(token, appConfig.secret);
-
-  return id;
-};
-
-export { createToken, reverseToken };
+export default createToken;
