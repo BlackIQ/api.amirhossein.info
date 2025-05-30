4cb6a3fae52bd70fe56bed23e6fcc2b9edb6a687
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-05-30 19:40:28 +0330

    Update config

--- app/config/app/app.config.js
+++ app/config/app/app.config.js
@@ -3,6 +3,6 @@ import env from "$app/env/index.js";
 export default {
   environment: env.APP_ENVIRONMENT,
   port: env.APP_PORT,
-  secret: env.APP_SECRET,
+  key: env.APP_KEY,
   published: env.APP_ENVIRONMENT === "production" ? true : false,
 };
